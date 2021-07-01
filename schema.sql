315aec726abbb19fe1e98ca8ac293d6f6781bf47
Neil Dudani <neil.dudani@gmail.com>
2021-07-01 14:12:21 -0400

    changed file paths

--- schema.sql
+++ schema.sql
@@ -59,22 +59,22 @@ CREATE TABLE characteristic_reviews (
 -- LOAD INTO DATABASE USING COPY METHOD
 
 COPY photos(photo_id, review_id, url)
-FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/SDC/csv_data/reviews_photos.csv'
+FROM '/usr/share/app/reviews_photos.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY characteristics(characteristic_id, product_id, characteristic)
-FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/SDC/csv_data/characteristics.csv'
+FROM '/usr/share/app/characteristics.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY reviews(review_id, product_id, rating, date, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulnessCount)
-FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/SDC/csv_data/reviews.csv'
+FROM '/usr/share/app/reviews.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY characteristic_reviews(id, characteristic_id, review_id, rating)
-FROM '/Users/neildudani/Desktop/Neil/HackReactor/Immersive/Projects/SDC/csv_data/characteristic_reviews.csv'
+FROM '/usr/share/app/characteristic_reviews.csv'
 DELIMITER ','
 CSV HEADER;
 
